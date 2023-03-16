3c11a1c498584c4ab0ae44462bb0499e35fe6f12
Jasmyne <103139683+jasmynels@users.noreply.github.com>
2023-03-16 15:00:09 -0300

    feat: created Person

---
+++ exercicio-1/ex-1/Person.cs
@@ -0,0 +1,8 @@
+namespace Course
+{
+    class Person
+    {
+        public string Nome;
+        public int Idade;
+    }
+}
---
+++ exercicio-1/ex-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Course;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Person person1, person2;
+            person1 = new Person();
+            person2 = new Person();
+            Console.WriteLine("Digite o nome da primeira pessoa e em seguida a idade dela: ");
+            person1.Nome = Console.ReadLine();
+            person1.Idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o nome da segunda pessoa e em seguida a idade dela: ");
+            person2.Nome = Console.ReadLine();
+            person2.Idade = int.Parse(Console.ReadLine());
+
+            if (person1.Idade > person2.Idade)
+            {
+                Console.WriteLine($"{person1.Nome} é mais velho(a)");
+            } else {
+                Console.WriteLine($"{person2.Nome} é mais velho(a)");
+            }
+        }
+    }
+}
